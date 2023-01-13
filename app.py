5ab065d18f8cf651f8842f4e7c54eaac6c91a537
lucasbogo <lucasbogo93@gmail.com>
2023-01-13 09:45:17 -0300

    understanding flask

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, session, g
-from database import Database
+from database import DbConnection
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
@@ -18,7 +18,7 @@ def get_current_user():
     user_result = None  # global variable set to none
     if 'user' in session:
         user = session['user']  # get user from session and store in variable
-        db = Database().connect()  # connect to database
+        db = DbConnection().get_connection()
         # execute query to check if user exists in the database
         user_cursor = db.execute("SELECT * FROM users WHERE name = ?", [user])
         user_result = user_cursor.fetchone()  # fetch the result of the query
@@ -36,25 +36,23 @@ def login():
     user = get_current_user()
     error = None
     if request.method == "POST":
-        db = Database().connect()
+        db = DbConnection().get_connection()
         name = request.form['name']
         password = request.form['password']
-
         # execute query to check if user exists in the database
         user_fetching_cursor = db.execute(
             "SELECT * FROM users WHERE name = ?", [name])
         existing_user = user_fetching_cursor.fetchone()
 
-        if existing_user:
+        if existing_user:  # check if user exists
+            # check if password is correct
             if check_password_hash(existing_user[2], password):
-                session['user'] = existing_user[1]
+                session['user'] = existing_user[1]  # set session user to name
                 return redirect(url_for('index'))
             else:
-                error = "Username or password is incorrect"
-                # return render_template("login.html", error=error)
+                error = "Invalid password"  # return error message if password is wrong
         else:
-            error = "Username or password is incorrect"
-            # return redirect(url_for('login'))
+            error = "User does not exist"   # return error message if user does not exist
 
     return render_template("login.html", user=user, error=error)
 
@@ -64,7 +62,7 @@ def register():
     user = get_current_user()
     error = None
     if request.method == "POST":
-        db = Database().connect()
+        db = DbConnection().get_connection()
         name = request.form['name']
         password = request.form['password']
 
@@ -86,6 +84,60 @@ def register():
         return redirect(url_for('index'))
 
     return render_template("register.html", user=user)
+
+
+@app.route("/ask", methods=["POST", "GET"])
+def ask():
+    user = get_current_user()
+    db = DbConnection().get_connection()
+    if request.method == "POST":
+        question = request.form['question']
+        teacher = request.form['teacher']
+        db.execute("INSERT INTO questions (question, user_id, teacher_id) VALUES (?, ?, ?)", [
+                   question, user[0], teacher])
+        db.connection.commit()
+        return redirect(url_for('index'))
+    teacher_cursor = db.execute("SELECT * FROM users WHERE teacher = 1")
+    teachers = teacher_cursor.fetchall()
+    return render_template("ask.html", user=user, teachers=teachers)
+
+@app.route("/questions", methods=["POST", "GET"])
+def questions():
+    user = get_current_user()
+    db = DbConnection().get_connection()
+    question_cursor = db.execute(
+        "select questions.id, questions.question, users.name from questions join users on users.id = questions.user_id where questions.answer IS NULL and questions.teacher_id = ?", [user[0]])
+    questions = question_cursor.fetchall()
+    return render_template("questions.html", user=user, questions=questions)
+
+@app.route("/answer<question_id>", methods=["POST", "GET"])
+def answer(question_id):
+    user = get_current_user()
+    db = DbConnection().get_connection()
+    question_cursor = db.execute(
+        "select question from questions where id = ?", [question_id])
+    question = question_cursor.fetchall()
+    return render_template("answer.html", user=user, question_id=question_id, question=question)
+
+
+@app.route("/users", methods=["POST", "GET"])
+def users():
+    user = get_current_user()
+    db = DbConnection().get_connection()
+    user_cursor = db.execute("SELECT * FROM users")
+    users = user_cursor.fetchall()
+    return render_template("users.html", user=user, users=users)
+
+
+@app.route("/promote/<int:id>", methods=["POST", "GET"])
+def promote(id):
+    user = get_current_user()
+    if request.method == "GET":
+        db = DbConnection().get_connection()
+        db.execute("UPDATE users SET teacher = 1 WHERE id = ?", [id])
+        db.connection.commit()
+        return redirect(url_for('users', user=user))
+    return render_template("users.html", user=user)
 
 
 @app.route("/logout")
--- database.py
+++ database.py
@@ -3,25 +3,44 @@ import sqlite3
 
 # Singleton pattern to connect to the database only once    
 
-class MetaSingleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+# class MetaSingleton(type):
+#     _instances = {}
+#     def __call__(cls, *args, **kwargs):
+#         if cls not in cls._instances:
+#             cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
+#         return cls._instances[cls]
 
-class Database(metaclass=MetaSingleton):
-    connection = None
+class DbConnection:
+    _instance = None
 
-    def connect(self):
-        if self.connection is None:
-            self.connection = sqlite3.connect('/home/lucas/python_projects/quiz_app/quizapp.db')
-            self.cursorobj = self.connection.cursor()
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(DbConnection, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        self.connection = sqlite3.connect('/home/lucas/python_projects/quiz_app/quizapp.db')
+        self.cursorobj = self.connection.cursor()
+
+    def get_connection(self):
         return self.cursorobj
-
 
     def close(self):
         self.connection.close()
+        
+
+# class Database(metaclass=MetaSingleton):
+#     connection = None
+
+#     def connect(self):
+#         if self.connection is None:
+#             self.connection = sqlite3.connect('/home/lucas/python_projects/quiz_app/quizapp.db')
+#             self.cursorobj = self.connection.cursor()
+#         return self.cursorobj
+
+#     def close(self):
+#         self.connection.close()
+
 
 
     
